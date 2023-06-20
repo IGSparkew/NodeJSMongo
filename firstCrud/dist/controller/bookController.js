3da7437ab81e2965090287219d20ededdc8435e0
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2023-06-20 17:04:15 +0200

    fix gitignore et supression du dist fichier

--- firstCrud/dist/controller/bookController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BookController = void 0;
-class BookController {
-    getBook(req, res, next) {
-        res.send('get all the books');
-    }
-}
-exports.BookController = BookController;
